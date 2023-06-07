6e45d9f804978d362e52f2599f41e9069121aa92
Aman Vahora <Amanvahora1@gmail.com>
2023-06-06 18:59:26 -0700

    Set up initial connection to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+// const cors = require('cors')
+// app.use(cors());
+app.use(express.json())
+const db = mysql.createPool({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: 'aps'
+    
+})
+app.get("/", (req,res)=>{
+    const sqlInsert = "SELECT * FROM vehicle"
+    db.query(sqlInsert, (err, result) => {
+        if(err) {
+            console.log(err)
+            } 
+        res.send(result); 
+    })
+});
+
 
 app.listen(3001, () => {
 console.log("running on port 3001");
