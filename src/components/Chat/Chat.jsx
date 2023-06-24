1215ac31130f755303d3d3db9573c3c496907c87
Aman Vahora <Amanvahora1@gmail.com>
2023-06-24 12:36:26 -0700

    updated team page

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,12 +3,11 @@ import { useState } from 'react'
 import './Chat.scss'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
-import ChatBot from 'react-simple-chatbot';
-import { ThemeProvider } from 'styled-components';
 
 const API_KEY = "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";
 const systemMessage = { 
-  "role": "system", "content": "Act as an autoparts expert for an autoparts website. You should be able to answer questions related to various car makes, models, and autoparts. You should provide accurate and helpful information about different types of autoparts, their compatibility with specific vehicles, installation procedures, common issues, troubleshooting tips, and recommendations for quality autopart brands. You should also be knowledgeable about maintenance schedules, warranty information, and any specific requirements or considerations for different car models. The goal is to provide users with reliable autoparts advice and support, helping them make informed decisions and solve autoparts-related problems effectively."
+  "role": "system", 
+  "content": "Act as an autoparts expert for an autoparts website. You should be able to answer questions related to various car makes, models, and autoparts. You should provide accurate and helpful information about different types of autoparts, their compatibility with specific vehicles, installation procedures, common issues, troubleshooting tips, and recommendations for quality autopart brands. You should also be knowledgeable about maintenance schedules, warranty information, and any specific requirements or considerations for different car models. The goal is to provide users with reliable autoparts advice and support, helping them make informed decisions and solve autoparts-related problems effectively."
 }
 const Chat = () => {
     // const configuration = new Configuration({
@@ -40,11 +39,9 @@ const Chat = () => {
         setIsTyping(true);
         await processMessageToChatGPT(newMessages);
     };
-    async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-      // Format messages for chatGPT API
-      // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-      // So we need to reformat
+    async function processMessageToChatGPT(chatMessages) { 
   
+      
       let apiMessages = chatMessages.map((messageObject) => {
         let role = "";
         if (messageObject.sender === "ChatGPT") {
@@ -57,8 +54,8 @@ const Chat = () => {
       const apiRequestBody = {
         "model": "gpt-3.5-turbo",
         "messages": [
-          systemMessage,  // The system message DEFINES the logic of our chatGPT
-          ...apiMessages // The messages from our chat with ChatGPT
+          systemMessage,  
+          ...apiMessages 
         ]
       }
   
@@ -81,7 +78,6 @@ const Chat = () => {
         setIsTyping(false);
       });
     }
-    // floating button
     const [isOpen, setIsOpen] = useState(false);
 
     const handleToggle = () => {
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -8,13 +8,13 @@ function Team(){
         {
           name: 'Hillary Cline',
           image: Person1,
-          review: '"Shopping at Gear Head Garage has been a game-changer for all my automotive needs. From the moment I stepped into the store, I was greeted by a knowledgeable and friendly staff who were eager to assist me. Their expertise and passion for auto parts were evident, making me feel confident in my purchase decisions."',
+          review: '"Shopping at Gear Head Garage has been a game-changer for all my automotive needs. From the moment I stepped into the store, I was greeted by a knowledgeable and friendly staff whise and passion for auto parts were evident, making me feel confident in my purchase decisions."',
           position: 'General Manager'
         },
         {
           name: 'Jane Smith',
           image: Person2,
-          review: '"One aspect that sets Gear Head Garage apart is their commitment to customer satisfaction. The staff went above and beyond to ensure I found exactly what I needed. They patiently answered all my questions, provided valuable recommendations, and even offered helpful installation tips. Their dedication to exceptional service made the entire shopping experience smooth and enjoyable."',
+          review: '"One aspect that sets Gear Head Garage apart is their commitment to customer satisfaction. The staff went above and beyond to ensure I found exactly what I needed. They patiently answered all m"',
           position: 'Manager'
         },
 
@@ -28,7 +28,7 @@ function Team(){
 <div className="team-container">
     <div className="team-text">
         <h1>Check out what our customers have said about us.</h1>
-        <p>When you read our testimonies, you embark on a journey of trust-building. You can hear the voices of our loyal customers, sharing their stories and affirming their confidence in our brand. Each testimony is a reflection of the trust, credibility, and exceptional service that form the foundation of our business.</p>
+        <p>When you read our testimonies, you embark on a journey of trust-building. You can hear the voices of our loyal customers, sharing their stories and affirming their confidence in our brand. Each testimony .</p>
 </div>
 <div className="team-box">
 {testimonials.map((testimonial, index) => (
@@ -49,6 +49,7 @@ function Team(){
 const TeamBox = ({ name, image, review, position}) => {
     return (
       <div className="team-innerBox">
+        <div className="team-boxContent">
         <div className="imageBox">
         <img src={image} alt={name} className="testimonial-image" />
         </div>
@@ -56,8 +57,15 @@ const TeamBox = ({ name, image, review, position}) => {
           <span className="team-name">{name}</span>
           <span className="team-name">{position}</span>
         </div>
+        <div className="team-text">
         <p className="team-content">{review}</p>
-        
+        </div>
+        <div className="team-socials">
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-twitter"></i>
+        </div>
+        </div>
       </div>
     );
   };
