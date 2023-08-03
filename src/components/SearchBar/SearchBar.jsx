ba633d8a9a632ced787649742010aa57099a3912
Arashpreet123 <pandherarashpreet@gmail.com>
2023-08-03 12:18:43 -0700

    update

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import React from 'react'
 import "./SearchBar.scss"
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar ({data}) {
-
+    const[searchInput, setSearchInput] = useState('');
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        console.log(searchValue)
+    }
     return <>
         <div className="searchBar">
             <div className="searchInputs">
-                <input className="searchInput" type="text" placeholder="Enter Search Input" />
+                <input className="searchInput" placeholder="Enter Search Input"
+                onChange={(e) => searchItems(e.target.value)} />
                 <div className="searchIcons">
                     <SearchIcon></SearchIcon>
 
