89860bc6c9bf1e77b27c02a594b120952f4a2d6a
Arashpreet123 <pandherarashpreet@gmail.com>
2023-08-15 16:38:11 -0700

    update

---
+++ src/components/productListing/ProuductListing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./ProuductListing.scss"
+// import { useState } from 'react'
+
+// const[searchInput, seSearchInput] = useState("");
+const ProuductListing = () => {
+    return <>
+        <div className="searchBar">
+
+        </div> 
+    </>
+
+}
+
+export default ProuductListing;
