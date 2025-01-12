467de0bcb61116748bc13a94da4eaa04f024d824
Arashpreet123 <pandherarashpreet@gmail.com>
2025-01-11 20:17:04 -0800

    Update .gitignore

--- server/index.js
+++ server/index.js
@@ -28,7 +28,19 @@ app.use(session({
   app.use(passport.session())
   app.use(flash())
 
-  
+  const path = require('path');
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, process.env.CA_CERT_PATH)),  // Resolve relative to the server folder
+    },
+});
+
 
 var exists = "";
 app.post('/register',  async (req,res)=>{
